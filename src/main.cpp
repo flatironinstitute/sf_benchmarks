01176fd9cef88772c46281782e336e75a41cd71b
Robert Blackwell <rblackwell@flatironinstitute.org>
2022-04-28 10:17:46 -0400

    Add some more functions to test

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include "boost/math/special_functions/math_fwd.hpp"
+#include "gsl/gsl_sf_gamma.h"
 #include <cstdlib>
 #include <iomanip>
 #include <ios>
@@ -78,10 +80,39 @@ BenchResult test_func(const std::string name, const std::string library_prefix,
     return res;
 }
 
+double boost_bessel_Y0(double x) { return boost::math::cyl_neumann(0, x); }
+double boost_bessel_K0(double x) { return boost::math::cyl_bessel_k(0, x); }
+double boost_bessel_I0(double x) { return boost::math::cyl_bessel_i(0, x); }
+double boost_bessel_J0(double x) { return boost::math::cyl_bessel_j(0, x); }
+double boost_bessel_j0(double x) { return boost::math::sph_bessel(0, x); }
+double boost_bessel_y0(double x) { return boost::math::sph_neumann(0, x); }
+
 int main(int argc, char *argv[]) {
-    std::unordered_map<std::string, fun_1d> gsl_funs = {{"tgamma", gsl_sf_gamma}};
-    std::unordered_map<std::string, fun_1d> boost_funs = {{"tgamma", boost::math::tgamma<double>}};
-    std::unordered_map<std::string, fun_1d> std_funs = {{"tgamma", std::tgamma}};
+    std::unordered_map<std::string, fun_1d> gsl_funs = {{"tgamma", gsl_sf_gamma},
+                                                        {"lgamma", gsl_sf_lngamma},
+                                                        {"bessel_Y0", gsl_sf_bessel_Y0},
+                                                        {"bessel_I0", gsl_sf_bessel_I0},
+                                                        {"bessel_J0", gsl_sf_bessel_J0},
+                                                        {"bessel_K0", gsl_sf_bessel_K0},
+                                                        {"bessel_j0", gsl_sf_bessel_j0},
+                                                        {"bessel_y0", gsl_sf_bessel_y0}
+    };
+    std::unordered_map<std::string, fun_1d> boost_funs = {{"tgamma", boost::math::tgamma<double>},
+                                                          {"lgamma", boost::math::lgamma},
+                                                          {"erf", boost::math::erf},
+                                                          {"erfc", boost::math::erfc},
+                                                          {"bessel_Y0", boost_bessel_Y0},
+                                                          {"bessel_I0", boost_bessel_I0},
+                                                          {"bessel_J0", boost_bessel_J0},
+                                                          {"bessel_K0", boost_bessel_K0},
+                                                          {"bessel_j0", boost_bessel_j0},
+                                                          {"bessel_y0", boost_bessel_y0},
+    };
+    std::unordered_map<std::string, fun_1d> std_funs = {{"tgamma", std::tgamma},
+                                                        {"lgamma", std::lgamma},
+                                                        {"erf", std::erf},
+                                                        {"erfc", std::erfc},
+    };
     std::unordered_map<std::string, fun_1d> sctl_funs;
     std::unordered_map<std::string, fun_1d> sleef_funs;
 
@@ -97,7 +128,7 @@ int main(int argc, char *argv[]) {
     for (auto kv : sleef_funs)
         fun_union.insert(kv.first);
 
-    std::vector<double> vals(10000000);
+    std::vector<double> vals(1000000);
     srand(100);
     for (auto &val : vals)
         val = rand() / (double)RAND_MAX;
@@ -108,5 +139,6 @@ int main(int argc, char *argv[]) {
         std::cout << test_func(key, "sleef", sleef_funs, vals) << std::endl;
         std::cout << test_func(key, "std", std_funs, vals) << std::endl;
         std::cout << test_func(key, "sctl", sctl_funs, vals) << std::endl;
+        std::cout << "\n\n";
     }
 }
