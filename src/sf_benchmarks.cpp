45687f8c7d2bc7be59ede04416fd1b20e3e5c8de
Robert Blackwell <rblackwell@flatironinstitute.org>
2022-05-15 13:03:59 -0400

    Add clang detection

--- src/sf_benchmarks.cpp
+++ src/sf_benchmarks.cpp
@@ -12,7 +12,11 @@ host_info_t::host_info_t() {
 }
 
 toolchain_info_t::toolchain_info_t() {
-#ifdef __GNUC__
+#if defined(__clang__)
+    compiler = "clang";
+    compilervers =
+        std::to_string(__clang_major__) + "." + std::to_string(__clang_minor__) + "." + std::to_string(__clang_patchlevel__);
+#elif defined(__GNUC__)
     compiler = "gcc";
     compilervers =
         std::to_string(__GNUC__) + "." + std::to_string(__GNUC_MINOR__) + "." + std::to_string(__GNUC_PATCHLEVEL__);
