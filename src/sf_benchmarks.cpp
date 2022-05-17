c6266fa9e42c4aa6d6658eda0b83fc3322463efb
Robert Blackwell <rblackwell@flatironinstitute.org>
2022-05-17 04:08:45 -0400

    Use sysconf(3) to get cache information

--- src/sf_benchmarks.cpp
+++ src/sf_benchmarks.cpp
@@ -2,20 +2,21 @@
 #include <sf_utils.hpp>
 
 #include <gnu/libc-version.h>
+#include <unistd.h>
 
 host_info_t::host_info_t() {
     cpuname = sf::utils::exec("grep -m1 'model name' /proc/cpuinfo | cut -d' ' --complement -f1-3");
-    L1d = sf::utils::exec("lscpu | grep L1d | awk '{print $3}'");
-    L1i = sf::utils::exec("lscpu | grep L1i | awk '{print $3}'");
-    L2 = sf::utils::exec("lscpu | grep L2 | awk '{print $3}'");
-    L3 = sf::utils::exec("lscpu | grep L3 | awk '{print $3}'");
+    L1d = std::to_string(sysconf(_SC_LEVEL1_DCACHE_SIZE));
+    L1i = std::to_string(sysconf(_SC_LEVEL1_ICACHE_SIZE));
+    L2 = std::to_string(sysconf(_SC_LEVEL2_CACHE_SIZE));
+    L3 = std::to_string(sysconf(_SC_LEVEL3_CACHE_SIZE));
 }
 
 toolchain_info_t::toolchain_info_t() {
 #if defined(__clang__)
     compiler = "clang";
-    compilervers =
-        std::to_string(__clang_major__) + "." + std::to_string(__clang_minor__) + "." + std::to_string(__clang_patchlevel__);
+    compilervers = std::to_string(__clang_major__) + "." + std::to_string(__clang_minor__) + "." +
+                   std::to_string(__clang_patchlevel__);
 #elif defined(__GNUC__)
     compiler = "gcc";
     compilervers =
