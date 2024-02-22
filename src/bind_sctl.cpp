02aea37608d5acef56330e4d1be972784e69b065
Robert Blackwell <rblackwell@flatironinstitute.org>
2024-02-22 08:30:21 -0500

    Add sctl's new log variant

--- src/bind_sctl.cpp
+++ src/bind_sctl.cpp
@@ -5,15 +5,16 @@ namespace sf::functions::SCTL {
 std::unordered_map<std::string, multi_eval_func<float>> funs_fx8 = {
     {"memcpy", sctl_apply<float, 8>([](const sctl_fx8 &x) { return x; })},
     {"memset", sctl_apply<float, 8>([](const sctl_fx8 &x) -> sctl_fx8 { return (sctl_fx8::VData)Vec8f{0.0}; })},
-    {"exp", sctl_apply<float, 8>([](const sctl_fx8 &x) { return sctl::approx_exp<7>(x); })},
+    {"exp", sctl_apply<float, 8>([](const sctl_fx8 &x) { return exp(x); })},
+    {"log", sctl_apply<float, 8>([](const sctl_fx8 &x) { return log(x); })},
     {"sin", sctl_apply<float, 8>([](const sctl_fx8 &x) {
          sctl_fx8 sinx, cosx;
-         sctl::approx_sincos<7>(sinx, cosx, x);
+         sincos(sinx, cosx, x);
          return sinx;
      })},
     {"cos", sctl_apply<float, 8>([](const sctl_fx8 &x) {
          sctl_fx8 sinx, cosx;
-         sctl::approx_sincos<7>(sinx, cosx, x);
+         sincos(sinx, cosx, x);
          return cosx;
      })},
     {"rsqrt", sctl_apply<float, 8>([](const sctl_fx8 &x) { return sctl::approx_rsqrt<7>(x); })},
@@ -22,15 +23,16 @@ std::unordered_map<std::string, multi_eval_func<float>> funs_fx8 = {
 std::unordered_map<std::string, multi_eval_func<double>> funs_dx4 = {
     {"memcpy", sctl_apply<double, 4>([](const sctl_dx4 &x) { return x; })},
     {"memset", sctl_apply<double, 4>([](const sctl_dx4 &x) -> sctl_dx4 { return (sctl_dx4::VData)Vec4d{0.0}; })},
-    {"exp", sctl_apply<double, 4>([](const sctl_dx4 &x) { return sctl::approx_exp<16>(x); })},
+    {"exp", sctl_apply<double, 4>([](const sctl_dx4 &x) { return exp(x); })},
+    {"log", sctl_apply<double, 4>([](const sctl_dx4 &x) { return log(x); })},
     {"sin", sctl_apply<double, 4>([](const sctl_dx4 &x) {
          sctl_dx4 sinx, cosx;
-         sctl::approx_sincos<16>(sinx, cosx, x);
+         sincos(sinx, cosx, x);
          return sinx;
      })},
     {"cos", sctl_apply<double, 4>([](const sctl_dx4 &x) {
          sctl_dx4 sinx, cosx;
-         sctl::approx_sincos<16>(sinx, cosx, x);
+         sincos(sinx, cosx, x);
          return cosx;
      })},
     {"rsqrt", sctl_apply<double, 4>([](const sctl_dx4 &x) { return sctl::approx_rsqrt<16>(x); })},
@@ -40,15 +42,16 @@ std::unordered_map<std::string, multi_eval_func<double>> funs_dx4 = {
 std::unordered_map<std::string, multi_eval_func<float>> funs_fx16 = {
     {"memcpy", sctl_apply<float, 16>([](const sctl_fx16 &x) { return x; })},
     {"memset", sctl_apply<float, 16>([](const sctl_fx16 &x) -> sctl_fx16 { return (sctl_fx16::VData)Vec16f{0.0}; })},
-    {"exp", sctl_apply<float, 16>([](const sctl_fx16 &x) { return sctl::approx_exp<7>(x); })},
+    {"exp", sctl_apply<float, 16>([](const sctl_fx16 &x) { return exp(x); })},
+    {"log", sctl_apply<float, 16>([](const sctl_fx16 &x) { return log(x); })},
     {"sin", sctl_apply<float, 16>([](const sctl_fx16 &x) {
          sctl_fx16 sinx, cosx;
-         sctl::approx_sincos<7>(sinx, cosx, x);
+         sincos(sinx, cosx, x);
          return sinx;
      })},
     {"cos", sctl_apply<float, 16>([](const sctl_fx16 &x) {
          sctl_fx16 sinx, cosx;
-         sctl::approx_sincos<7>(sinx, cosx, x);
+         sincos(sinx, cosx, x);
          return cosx;
      })},
     {"rsqrt", sctl_apply<float, 16>([](const sctl_fx16 &x) { return sctl::approx_rsqrt<7>(x); })},
@@ -57,15 +60,16 @@ std::unordered_map<std::string, multi_eval_func<float>> funs_fx16 = {
 std::unordered_map<std::string, multi_eval_func<double>> funs_dx8 = {
     {"memcpy", sctl_apply<double, 8>([](const sctl_dx8 &x) { return x; })},
     {"memset", sctl_apply<double, 8>([](const sctl_dx8 &x) -> sctl_dx8 { return (sctl_dx8::VData)Vec8d{0.0}; })},
-    {"exp", sctl_apply<double, 8>([](const sctl_dx8 &x) { return sctl::approx_exp<16>(x); })},
+    {"exp", sctl_apply<double, 8>([](const sctl_dx8 &x) { return exp(x); })},
+    {"log", sctl_apply<double, 8>([](const sctl_dx8 &x) { return log(x); })},
     {"sin", sctl_apply<double, 8>([](const sctl_dx8 &x) {
          sctl_dx8 sinx, cosx;
-         sctl::approx_sincos<16>(sinx, cosx, x);
+         sincos(sinx, cosx, x);
          return sinx;
      })},
     {"cos", sctl_apply<double, 8>([](const sctl_dx8 &x) {
          sctl_dx8 sinx, cosx;
-         sctl::approx_sincos<16>(sinx, cosx, x);
+         sincos(sinx, cosx, x);
          return cosx;
      })},
     {"rsqrt", sctl_apply<double, 8>([](const sctl_dx8 &x) { return sctl::approx_rsqrt<16>(x); })},
